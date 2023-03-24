6fb55b01cbba9e7920b7f324d91685fbb5730331
click-SK <sashakushnir4991@gmail.com>
2023-03-24 21:46:24 +0200

    add_admin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import CalculatePublick from './component/CalculatePublick.js';
+import AdminPanel from './component/AdminPanel.js';
 
 function App() {
   return (
     <div className="App">
       <CalculatePublick/>
+      <AdminPanel/>
     </div>
   );
 }
