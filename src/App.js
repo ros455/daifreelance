51d8f07fdb66c3e72414cff9ff2a9d347b9db6b8
click-SK <sashakushnir4991@gmail.com>
2023-03-25 19:52:48 +0200

    animation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import CalculatePublick from './component/CalculatePublick.js';
 import AdminPanel from './component/AdminPanel.js';
 import Table from './component/Table';
+import AnimWave from './component/AnimWave';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <CalculatePublick/>
       <AdminPanel/>
       {/* <Table/> */}
+      {/* <AnimWave/> */}
     </div>
   );
 }
