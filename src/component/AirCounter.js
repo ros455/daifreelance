98919110bcf03e24ce683d762485329b9e0f68d2
Kichuk Rostyslav <ros.kichuk@gmail.com>
2023-03-26 13:28:16 +0300

    add component air

---
+++ src/component/AirCounter.js
@@ -0,0 +1,24 @@
+import React from 'react';
+const AirCounter = ({lastAir, angle}) => {
+    return (
+        <>
+            <div className='air__speed'>
+                <div className='back__big-circle'></div>
+                <div className='eclipse-gradient'></div>
+                <div className='eclipse-gradient-second'></div>
+                <div className='eclipse-speed'></div>
+            </div>
+            <div className='back__small-circle'></div>
+            <div className='debug'>
+                {lastAir && lastAir}
+            </div>
+            <div className='circule-absolute'>
+                <div className='circle'>
+                    <div className="dot" style={{ transform: `rotate(${angle ? angle : 210}deg)` }}></div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default AirCounter;
--- src/component/CalculatePublick.js
+++ src/component/CalculatePublick.js
@@ -5,6 +5,7 @@ import '../style/style-null.css'
 import '../style/circule.scss'
 import CounterYear from './CounterYear';
 import Counter from './Counter';
+import AirCounter from './AirCounter';
 
 
 const CalculatePublick = () => {
@@ -144,21 +145,7 @@ const CalculatePublick = () => {
                         <div className='calc__wrap_row'>
                             <div className='calc__wrap-item air'>
                                 <h2>Air</h2>
-                                <div className='air__speed'>
-                                    <div className='back__big-circle'></div>
-                                    <div className='eclipse-gradient'></div>
-                                    <div className='eclipse-gradient-second'></div>
-                                    <div className='eclipse-speed'></div>
-                                </div>
-                                    <div className='back__small-circle'></div>
-                                    <div className='debug'>
-                                        {lastAir && lastAir}
-                                    </div>
-                                    <div className='circule-absolute'>
-                                        <div className='circle'>
-                                        <div className="dot" style={{ transform: `rotate(${angle}deg)` }}></div>
-                                    </div>
-                                    </div>
+                                <AirCounter angle={angle} lastAir={lastAir}/>
                                 </div>
                             <div className='calc__wrap-item balance'>
                                 <h2>Balance</h2>
