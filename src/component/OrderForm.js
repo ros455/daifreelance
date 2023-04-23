d2fec401226d6367464687d8adb12d5b84338b0d
click-SK <sashakushnir4991@gmail.com>
2023-04-23 16:34:53 +0300

    add then form

--- src/component/OrderForm.js
+++ src/component/OrderForm.js
@@ -31,10 +31,19 @@ function OrderForm() {
       },
       body: JSON.stringify(order),
       mode: 'no-cors'
-    });
-
-    const data = await response.json();
-    console.log(data);
+    })
+    .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
   };
 
   return (
