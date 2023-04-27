4faff37f950fc00a3b66693ae1e2b6ea561b4a8a
click-SK <sashakushnir4991@gmail.com>
2023-04-27 15:26:19 +0300

    edit order ipa

--- src/component/OrderForm.js
+++ src/component/OrderForm.js
@@ -8,75 +8,73 @@ function OrderForm() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // const order = {
-    //   source_id: 1,
-    //   buyer: {
-    //     full_name: name,
-    //     phone: phone,
-    //   },
-    //   products: [
-    //     {
-    //       price: price,
-    //       quantity: 1,
-    //       name: "Product"
-    //     },
-    //   ],
-    // };
 
     const order = {
-      source_id: 10,
-      buyer: {
-        full_name: "Test sklo Kalculator",
-        phone: 2332234
-      },
-      products: [
-        {
-          price: 4000
-        },
-      ],
+      "source_id": 10,
+      "source_uuid": "115",
+      "buyer_comment": "I want this sentence to be my `buyer` comment on KeyCRM",
+      "manager_id": 1,
+      "manager_comment": "Handle till weekend",
+      "promocode": "MERRYCHRISTMAS",
+      "discount_percent": 11.5,
+      "discount_amount": 9.99,
+      "shipping_price": 2.5,
+      "wrap_price": 3.5,
+      "taxes": 2.5,
+      "ordered_at": "2021-12-21 14:44:00",
+      "buyer": {
+        "full_name": "John Doe",
+        "email": "john.doe@mail.app",
+        "phone": "+1 555-234-1234"
+      }
     };
-    // const order = {
-    //   "source_id": 1,
-    //   "buyer": {
-    //     "full_name": "Alex",
-    //     "phone": "+380662732124"
-    //   },
-    //   "products": [
-    //     {
-    //       "price": price,
-    //       "quantity": 1,
-    //       "name": "Product"
-    //     }
-    //   ]
-    // };
+    
+    const dataString = JSON.stringify(order);
+
 
     const token = 'ODQ0MDA5YjE3ZmJhMGYwNzQxMTFlN2FmYmRlZjE0MzEwNDljYzM5OQ'
 
-    const response = await fetch("https://openapi.keycrm.app/v1/order", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "Cache-Control": "no-cache",
-        "Pragma": "no-cache",
-        "Authorization": "Bearer ODQ0MDA5YjE3ZmJhMGYwNzQxMTFlN2FmYmRlZjE0MzEwNDljYzM5OQ"
-      },
-      body: JSON.stringify(order),
-      mode: 'no-cors'
-    })
-    .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log(data);
-      })
-      .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-      });
-  };
+  //   const response = await fetch("https://openapi.keycrm.app/v1/order", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "Accept": "application/json",
+  //       "Cache-Control": "no-cache",
+  //       "Pragma": "no-cache",
+  //       "Authorization": `Bearer ${token}`
+  //     },
+  //     body: JSON.stringify(order),
+  //     mode: 'no-cors'
+  //   })
+  //   .then(response => {
+  //       if (!response.ok) {
+  //         throw new Error('Network response was not ok');
+  //       }
+  //       return response.json();
+  //     })
+  //     .then(data => {
+  //       console.log(data);
+  //     })
+  //     .catch(error => {
+  //       console.error('There was a problem with the fetch operation:', error);
+  //     });
+  // };
+
+  fetch('https://openapi.keycrm.app/v1/order', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Cache-Control': 'no-cache',
+      'Pragma': 'no-cache',
+      'Authorization': 'Bearer ' + token
+    },
+    body: dataString,
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+};
 
   return (
     <form onSubmit={handleSubmit}>
